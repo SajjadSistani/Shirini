ee9bb8fecd3aac9b2e8aefb908b4f55550049727
Saji <sajjadsistani79@gmail.com>
2023-07-29 23:14:32 +0330

    finall app

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const data = {
     "باغلوا",
     "نون خامه ای",
     "کیک",
-    "پشمک حاج عبدالله",
+    "حاج عبدالله",
     "زبون",
     "ناپلئونی",
     "نخودی",
@@ -37,27 +37,54 @@ const data = {
   ],
 };
 
-//Select Category
+//Eelements
 const category = document.querySelector(".category-container");
 const mainApp = document.querySelector(".main-container");
 
+//Functions
 const init = () => {
   mainApp.style.display = "none";
   const randomsort = () => Math.random() - 0.5;
   data.food.sort(randomsort);
+  data.drink.sort(randomsort);
+  data.pastry.sort(randomsort);
 };
 
+const updateItemsByCategory = (categoryData) => {
+  const items = document.querySelectorAll(".main-container .item");
+  items.forEach((el, i) => {
+    el.textContent = categoryData[i];
+  });
+};
+
+//Calling Functions
 init();
 
+//Events
 category.addEventListener("click", (e) => {
   if (e.target.classList.contains("item")) {
     category.style.display = "none";
     mainApp.style.display = "flex";
     if (e.target.textContent === "غذا") {
-      const items = document.querySelectorAll(".main-container .item");
-      items.forEach((el, i) => {
-        el.textContent = data.food[i];
-      });
+      updateItemsByCategory(data.food);
+    }
+    if (e.target.textContent === "نوشیدنی") {
+      updateItemsByCategory(data.drink);
+    }
+    if (e.target.textContent === "شیرینی") {
+      updateItemsByCategory(data.pastry);
     }
   }
 });
+
+const handleClick = (e) => {
+  if (e.target.classList.contains("item")) {
+    e.target.classList.remove("item-covered");
+    const otherElements = document.querySelectorAll(".main-container .item");
+    Array.from(otherElements)
+      .filter((el) => el !== e.target)
+      .forEach((el) => el.classList.add("item-blured"));
+    mainApp.removeEventListener("click", handleClick);
+  }
+};
+mainApp.addEventListener("click", handleClick);
